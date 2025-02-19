7c2a1a065d6a2c1fe6813e645e6187bd4ede026c
Jagaa <jagaa99a@gmail.com>
2025-02-20 00:42:19 +0900

    Initial commit

---
+++ JapaneseLearning/JapaneseLearningApp.swift
@@ -0,0 +1,17 @@
+//
+//  JapaneseLearningApp.swift
+//  JapaneseLearning
+//
+//  Created by Jargalsaikhan Amgalan on 2025/02/20.
+//
+
+import SwiftUI
+
+@main
+struct JapaneseLearningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
